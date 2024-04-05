3cad721c1a6c478644c97c4409085c0ef5310597
Shivar Vishal Pillay <shivar.vishal.pillay@gmail.com>
2024-04-05 00:48:03 -0700

    yarn run build

---
+++ pages/profile.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import React, { useState } from 'react';
+
+import Body from "@/components/profile/Body";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Profile() {
+  return (
+    <div>
+
+    </div>
+  );
+}
