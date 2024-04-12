cf0e713b0705d66cc43a22539df31757693e82b3
Shivar Vishal Pillay <shivar.vishal.pillay@gmail.com>
2024-04-11 18:49:59 -0700

    updated site changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
+import { useRouter } from 'next/router';
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const isProfilePage = router.pathname.startsWith('/profile');
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      {!isProfilePage && <Navbar />}
       <main className="flex-grow">
         <Component {...pageProps} />
       </main>
-      <Footer />
+      {!isProfilePage && <Footer />}
     </div>
   );
 }
