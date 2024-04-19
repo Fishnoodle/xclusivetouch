160d53cb80e14c5ac0dffbb6eebd1850794dbc98
Shivar Vishal Pillay <shivar.vishal.pillay@gmail.com>
2024-04-18 18:51:34 -0700

    Update Body.js - Phone

--- components/profile/Body.js
+++ components/profile/Body.js
@@ -1,13 +1,19 @@
 import { React, useState } from 'react'
 import Header from "@/components/profile/Header";
+import Create from "@/components/profile/Create";
 
 const Body = (id) => {
 
+    // If id exists, return header component. if not, return create
 
     return (
         <div>
         <Header id={id}/>
         </div>
+        
+        <div>
+        <Create />
+        <div>
     )
 }
 
