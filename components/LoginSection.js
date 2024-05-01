5b870977016a87c729e17d624ed5ce4d92de3844
Shivar Vishal Pillay <shivar.vishal.pillay@gmail.com>
2024-04-30 23:48:39 -0700

    mobile test update

--- components/LoginSection.js
+++ components/LoginSection.js
@@ -115,6 +115,7 @@ export default function LoginSection() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-[#D4AF37] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                 onClick={login}
+                onTouchStart={login}
               >
                 Sign in
               </button>
