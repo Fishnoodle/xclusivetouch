12177f5973081ceb17a71aaf18b2a60e972cc6bf
Shivar Vishal Pillay <shivar.vishal.pillay@gmail.com>
2024-10-12 19:23:32 -0700

    About - showMore / showLess

--- components/profile/Header.js
+++ components/profile/Header.js
@@ -23,6 +23,7 @@ const Header = (profile, profilePicture) => {
     const [profilePictureLink, setProfilePictureLink] = useState("")
 
     const aboutRef = useRef(null);
+    const [isExpanded, setIsExpanded] = useState(false);
     const [isMultiLine, setIsMultiLine] = useState(false);
 
     const iconMapping = {
@@ -100,77 +101,91 @@ const Header = (profile, profilePicture) => {
     
     console.log(profile)
 
+    const toggleExpand = () => {
+      setIsExpanded(!isExpanded);
+    };
+
     return (
         <div className="overflow-hidden relative">
-            <div style={{ backgroundColor: headerColour, width: '100vw', height: '200px' }}/>
-
-            <div className="w-[90%] h-[200px] md:h-[400px] -translate-y-1/2 mx-auto flex overflow-hidden rounded-lg shadow-2xl shadow-gray-900 mt-5 md:mt-10">
-                <div className="w-1/2 h-full relative">
-                    <Image
-                        src={profilePictureLink}
-                        alt="Profile"
-                        layout="fill"
-                        style={{ objectFit: 'cover' }}
-                    />
-                </div>
-
-                <div
-                    style={{ backgroundColor: cardColour }}
-                    className={`w-1/2 flex flex-col items-start justify-center pl-3 ${
-                        cardColour === '#000000' ? 'text-white' : 'text-black'
-                    }`}
-                >
-                    <div className="mb-2 pl-2">
-                        <span className="block text-[26px] md:text-[55px] font-semibold">
-                            {firstName} {lastName}
-                        </span>
-                    </div>
-                    <div className="pl-2">
-                        <span className="block text-[16px] md:text-[36px] font-light">{position}</span>
-                        <span className="block text-[16px] md:text-[36px] font-light">@ {company}</span>
-                    </div>
-                </div>
-
+          <div style={{ backgroundColor: headerColour, width: '100vw', height: '200px' }}/>
+        
+          <div className="w-[90%] h-[200px] md:h-[400px] -translate-y-1/2 mx-auto flex overflow-hidden rounded-lg shadow-2xl shadow-gray-900 mt-5 md:mt-10">
+            <div className="w-1/2 h-full relative">
+              <Image
+                src={profilePictureLink}
+                alt="Profile"
+                layout="fill"
+                style={{ objectFit: 'cover' }}
+              />
             </div>
-
-            <div className="w-[90%] top-[50%] flex justify-center items-center space-x-10 mx-auto -translate-y-20 mt-5 md:mt-10">
-                <Button fullWidth variant="outlined" size="lg" className="text-sm" onClick={createVCard}>
-                    <span className="text-[60%]">Save Contact</span>
-                </Button>
-                <Button fullWidth variant="gradient" size="lg" className="text-sm" onClick={() => window.location.href = `sms:${phoneNumber}`}>
-                    <span className="text-[60%]">Message Contact</span>
-                </Button>
+        
+            <div
+              style={{ backgroundColor: cardColour }}
+              className={`w-1/2 flex flex-col items-start justify-center pl-3 ${
+                cardColour === '#000000' ? 'text-white' : 'text-black'
+              }`}
+            >
+              <div className="mb-2 pl-2">
+                <span className="block text-[26px] md:text-[55px] font-semibold">
+                  {firstName} {lastName}
+                </span>
+              </div>
+              <div className="pl-2">
+                <span className="block text-[16px] md:text-[36px] font-light">{position}</span>
+                <span className="block text-[16px] md:text-[36px] font-light">@ {company}</span>
+              </div>
             </div>
-
-            <div className={`mx-5 -translate-y-1/2 ${isMultiLine ? 'mt-20 md:mt-24' : 'mt-10 md:mt-12'} flex flex-col space-y-5`}>
+          </div>
+        
+          <div className="w-[90%] mx-auto -mt-16">
+            <div className="flex justify-center items-center space-x-10">
+              <Button fullWidth variant="outlined" size="lg" className="text-sm" onClick={createVCard}>
+                <span className="text-[60%]">Save Contact</span>
+              </Button>
+              <Button fullWidth variant="gradient" size="lg" className="text-sm" onClick={() => window.location.href = `sms:${phoneNumber}`}>
+                <span className="text-[60%]">Message Contact</span>
+              </Button>
+            </div>
+            <div className="flex justify-center items-center mt-4">
+              <Button fullWidth variant="gradient" size="lg" className="text-sm">
+                <span className="text-[60%]">Exchange Profile</span>
+              </Button>
+            </div>
+          </div>
+        
+          <div className={`w-[90%] mx-auto mt-8 ${isMultiLine ? 'mt-20 md:mt-24' : 'mt-10 md:mt-12'} flex flex-col space-y-5`}>
             {companyAddress && (
-                <div>
-                    <p className="text-2xl font-bold mb-3">Company Address</p>
-                    <p>{companyAddress}</p>
-                </div>
+              <div>
+                <p className="text-2xl font-bold mb-3">Company Address</p>
+                <p>{companyAddress}</p>
+              </div>
             )}
-
-                <div>
-                    <p className="text-2xl font-bold mb-3">About</p>
-                    <div ref={aboutRef} dangerouslySetInnerHTML={{ __html: about.replace(/\n/g, '<br />') }} />
-                </div>
+        
+          <div>
+              <p className="text-2xl font-bold mb-3">About</p>
+              <div className={`relative ${isExpanded ? '' : 'line-clamp-2'}`}>
+                {about}
+              </div>
+              <button onClick={toggleExpand} className="text-gold mt-2">
+                {isExpanded ? 'Show Less' : 'Show More'}
+              </button>
             </div>
-
-            <div className="mx-5 -translate-y-1/5 mt-5 md:mt-10">
-                <p className="text-2xl font-bold mb-3"> Social Media Links </p>
-                <div className="flex space-x-4 overflow-x-auto">
-                    {Object.entries(socialLinks).map(([name, link]) => (
-                        <div key={name} className="w-16 h-16 bg-gray-200 rounded-2xl flex-none items-center justify-center">
-                            <a href={link.startsWith('http://') || link.startsWith('https://') ? link : `http://${link}`} target="_blank" rel="noopener noreferrer" className="w-full h-full flex items-center justify-center">
-                                {iconMapping[name.toLowerCase()] || name}
-                            </a>
-                        </div>
-                    ))}
+          </div>
+        
+          <div className="w-[90%] mx-auto mt-5 md:mt-10">
+            <p className="text-2xl font-bold mb-3">Social Media Links</p>
+            <div className="flex space-x-4 overflow-x-auto">
+              {Object.entries(socialLinks).map(([name, link]) => (
+                <div key={name} className="w-16 h-16 bg-gray-200 rounded-2xl flex-none items-center justify-center">
+                  <a href={link.startsWith('http://') || link.startsWith('https://') ? link : `http://${link}`} target="_blank" rel="noopener noreferrer" className="w-full h-full flex items-center justify-center">
+                    {iconMapping[name.toLowerCase()] || name}
+                  </a>
                 </div>
+              ))}
             </div>
-
+          </div>
         </div>
-    )
+      );
 }
 
 export default Header
